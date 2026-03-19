f606d7ca81621816450340c80f69831cb47d9cf6
axie <axie@atlassian.com>
2026-03-19 15:07:13 +1100

    package doc for java doc check

---
+++ server/src/main/java/org/opensearch/index/store/remote/segment/archive/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/**
+ * Archive utilities for remote store segment files.
+ */
+package org.opensearch.index.store.remote.segment.archive;
+
